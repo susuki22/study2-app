5cc806b252543d1fd896d6e5cda1133f24fa1b6f
susuki22 <shoya.s.0427@gmail.com>
2022-03-09 07:27:33 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
