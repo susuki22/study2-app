1f5018017be43ba17120efffe6b61249123ebd60
susuki22 <shoya.s.0427@gmail.com>
2022-03-15 08:47:41 +0000

    index記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
